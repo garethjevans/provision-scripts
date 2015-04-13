9f58f3da8af9488db065b27790b22db24f7f4638
cagiti <caicooper82@gmail.com>
2015-04-13 10:04:22 +0100

    Update install_urbancode_agent.sh

--- install_urbancode_agent.sh
+++ install_urbancode_agent.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+agent_download_path="https://github.com/garethjevans/provision-scripts/blob/master/ibm-ucd-agent.zip?raw=true"
+agent_download_name=`basename ${agent_download_path} | sed 's/\(^[a-z\]*-[a-z]*-[a-z]*.zip\).*$/\1/'`
+
 installation_dir=/opt/ibm-ucd/agent
 mkdir -p ${installation_dir}
-mv ibm-ucd-agent.zip /tmp
-unzip /tmp/ibm-ucd-agent.zip
-cd /tmp/ibm-ucd-agent-install
+
+wget ${agent_download_path} -O /tmp/${agent_download_name}
+unzip /tmp/${agent_download_name}
+cd /tmp/${agent_download_name}
 
 ./install_agent.sh << EOF
 ${installation_dir}
