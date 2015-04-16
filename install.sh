f3ab071c2a29b9510364a0d0aed34b2238866ad8
cagiti <caicooper82@gmail.com>
2015-04-16 14:31:10 +0100

    Update install.sh
    Changed approach to running as created user

--- install.sh
+++ install.sh
@@ -34,36 +34,28 @@ sed -i 's/^#\( %wheel.*NOPASSWD.*$\)/\1/' /etc/sudoers
 echo "adding user ${user} to the wheel group" >> $LOG
 usermod -aG wheel ${user}
 
-echo "Host Entries..." >> $LOG
-partial_ip=$(ifconfig | tail -n +`ifconfig | grep -n eth1| awk -F':' '{print $1+1}'` | head -1 | awk '{print substr($2,6,10)}')
-echo "${partial_ip}88 mcp-poc-uc.softlayer.com mcp-poc-uc" >> /etc/hosts
-echo "Done" >> $LOG
-
 # changing mode of the install log
 chmod 777 $LOG
-
-#change user to ${user}
-echo "switching user to ${user}" >> $LOG
-su - ${user}
-
-LOG=/tmp/install.log
 
 agent_download_path="https://github.com/garethjevans/provision-scripts/blob/master/ibm-ucd-agent.zip?raw=true"
 agent_download_name=`basename ${agent_download_path} | sed 's/\(^[a-z\]*-[a-z]*-[a-z]*.zip\).*$/\1/'`
 
 installation_dir=/opt/ibm-ucd/agent
+echo "Host Entries..." >> $LOG
 partial_ip=$(ifconfig | tail -n +`ifconfig | grep -n eth1| awk -F':' '{print $1+1}'` | head -1 | awk '{print substr($2,6,10)}')
+echo "${partial_ip}88 mcp-poc-uc.softlayer.com mcp-poc-uc" >> /etc/hosts
+echo "Done" >> $LOG
 host_name=$(grep `hostname` /etc/hosts | awk '{print $3}')
 
 echo "downloading ucd agent" >> $LOG
-sudo wget ${agent_download_path} -O /tmp/${agent_download_name} >> $LOG
+wget ${agent_download_path} -O /tmp/${agent_download_name} >> $LOG
 cd /tmp
 echo "unzipping /tmp/${agent_download_name}" >> $LOG
 unzip /tmp/${agent_download_name}
 cd /tmp/${agent_download_name%%.*}-install
 
 echo "running installing of ucd agent" >> $LOG
-sudo ./install-agent.sh << EOF
+./install-agent.sh << EOF
 ${installation_dir}
 Y
 /opt/java8
@@ -77,9 +69,8 @@ None
 EOF
 
 # configure access to ucd-agent installation
-echo "Changing owner of ${installation_dir}" >> $LOG
-sudo chown -R ${user} ${installation_dir}/..
-sudo chown -R :${user} ${installation_dir}/..
+echo "Changing owner of ${installation_dir} to ${user}" >> $LOG
+chown -R ${user} ${installation_dir}/..
+chown -R :${user} ${installation_dir}/..
 
-cd ${installation_dir}/bin
-./agent start >> $LOG
+sudo -H -u ${user} sh -c '/opt/ibm-ucd/agent/bin/agent start >> /tmp/install.log'
